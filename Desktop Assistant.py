c420a0df21d85a1bd9a0d03a6401845c0790085b
Sudip <duttasudip98@gmail.com>
2022-02-08 17:59:22 +0530

    First Commit

---
+++ Desktop Assistant.py
@@ -0,0 +1,148 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+import wolframalpha
+try:
+    import pywhatkit as kit
+except Exception as e:
+    print("cannot import module")
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        print("Good Morning!")
+        speak("Good Morning!")
+    elif hour>=12 and hour<18:
+        print("Good Afternoon!")
+        speak("Good Afternoon!")
+    else:
+        print("Good Evening!")
+        speak("Good Evening!")
+        
+    print("I am Sudip.Please tell me how can I help you")
+    speak("I am Sudip.Please tell me how can I help you")
+
+def takeCommand():
+    query=""
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")    
+
+    except Exception as e:
+        print("Say that again please...")
+        return "none"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your passward')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+
+if __name__ =="__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query,sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.get('C:/Program Files/Google/Chrome/Application/chrome.exe %s').open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.get('C:/Program Files/Google/Chrome/Application/chrome.exe %s').open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.get('C:/Program Files/Google/Chrome/Application/chrome.exe %s').open("stackoverflow.com")
+
+        elif 'open instagram' in query:
+            webbrowser.get('C:/Program Files/Google/Chrome/Application/chrome.exe %s').open("instagram.com")
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\Windows10\\OneDrive\\Desktop\\New Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Windows10\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to sudip' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "sudipyouremail@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry sir, I am not able to send this email")
+
+        elif 'play' in query:
+            try:
+                song_name = query
+                song_name_old = "play"
+                song_name_new = ""
+                song = song_name.replace(song_name_old, song_name_new)
+                if song == "none":
+                    print("none")
+                else:
+                    try:
+                        url = 'https://www.youtube.com/results?search_query=' + song
+                        kit.playonyt(url)
+                    except Exception as e:
+                        print("No internet!!")
+
+            except Exception as e:
+                print(e)
+                speak("Sorry sir, I am not able to Play")
+        else:
+            try:
+                question = query
+                app_id = "#######################"            
+                client = wolframalpha.Client(app_id)
+                res = client.query(question)
+                answer = next(res.results).text
+                print(answer)
+                speak(answer)
+            except Exception as e:
+                print("Nothing found! :(")
+
+
+
+
